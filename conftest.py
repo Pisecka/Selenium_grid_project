c63af318eba27b4e1d6c908c24cd947212b5bcc6
Karina Pisecka <63286923+Pisecka@users.noreply.github.com>
2023-12-03 12:49:30 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,9 @@
 import os
-
 import allure
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 allure
+
 
 def driver_factory(browser):
     if browser == "chrome":
@@ -20,14 +19,14 @@ def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
     parser.addoption("--b_version", default="117.0")
     parser.addoption("--url", default="https://demo.opencart.com/")
-    parser.addoption("--executor", action="store", default="http://192.168.0.3:4444")
+    parser.addoption("--executor", action="store", default="localhost")
 
 
 @pytest.fixture(scope="session", autouse=True)
 def get_environment(pytestconfig):
     props = {
         'Browser': 'Chrome',
-        'Browser.Version': '117.0',
+        'Browser.Version': '99.0',
         'Shell': os.getenv('SHELL')
     }
 
@@ -44,35 +43,47 @@ def browser(request):
     executor = request.config.getoption("--executor")
     url = request.config.getoption("--url")
 
-    if executor == "http://192.168.0.3:4444/wd/hub":
+    if executor == "local":
         driver = driver_factory(browser)
     else:
-        executor_url = f"http://192.168.0.3:4444/wd/hub"
-        caps = {
-            "browserName": browser,
-            "browserVersion": b_version,
-            "screenResolution": "1280x720",
-            "name": "Karina",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": False,
-                #     "enableLog": logs
-            },
-            'acceptSslCerts': True,
-            'acceptInsecureCerts': True,
-            'timeZone': 'Europe/Moscow',
-            'goog:chromeOptions': {}
-        }
+        executor_url = f"http://{executor}:4444/wd/hub"
+        # caps = {
+        #     "browserName": browser,
+        #     "browserVersion": b_version,
+        #     "screenResolution": "1280x720",
+        #     "name": "Karina",
+        #     "selenoid:options": {
+        #         "enableVNC": True,
+        #         "enableVideo": False,
+        #         #     "enableLog": logs
+        #     },
+        #     'acceptSslCerts': True,
+        #     'acceptInsecureCerts': True,
+        #     'timeZone': 'Europe/Moscow',
+        #     'goog:chromeOptions': {}
+        # }
         #if we want to write exactly which driver to use
         # options = Options()
         # driver = webdriver.Remote(
         #     command_executor=executor_url,
         #     options=webdriver.ChromeOptions()
         # )
+        options = webdriver.ChromeOptions()
+        options.set_capability("selenoid:options", {
+                "enableVNC": True,
+                "enableVideo": False,
+            })
+        options.set_capability("browserName", browser)
+        options.set_capability("browserVersion", b_version)
+        options.set_capability("screenResolution", "1280x720")
+        options.set_capability("name", "Karina")
+        options.set_capability("acceptSslCerts", True)
+        options.set_capability("acceptInsecureCerts", True)
+        options.set_capability("timeZone", "Europe/Moscow")
 
         driver = webdriver.Remote(
             command_executor=executor_url,
-            options=driver_factory(browser)
+            options=options
         )
 
     driver.url = url
